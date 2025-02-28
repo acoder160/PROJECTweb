9ac1138110fcbb7cc28eb1ebfa5251308f4faf1e
acoder160 <acoder160@gmail.com>
2025-02-28 13:32:33 +0100

    Forms provinces + town dropdown

--- formJs.js
+++ formJs.js
@@ -1,11 +1,10 @@
-// Getting all elements needed for an email
 var emailInput = document.getElementById("email");
 var validateButton = document.getElementById("Validate");
 var message = document.getElementById("validationMessage");
 
-// default message to "empty"
+
 message.style.display = "block";
-message.innerHTML = ""; // Start with an empty message
+message.innerHTML = ""; // we wil store the final message about email validation here
 
 validateButton.addEventListener("click", function () {
     var email = emailInput.value; 
@@ -21,5 +20,36 @@ validateButton.addEventListener("click", function () {
     } else {
         message.style.color = "red";
         message.innerHTML = email + " is incorrect :(";
+    }
+});
+
+// dropdown the proviences if navarra is selected 
+
+var countrySelect = document.getElementById("options");
+var provincesContainer = document.getElementById("container");
+
+
+countrySelect.addEventListener("change", function() {
+    if (countrySelect.value === "Spain") {
+        provincesContainer.style.display = "block";
+    } else {
+        provincesContainer.style.display = "none";
+    }
+});
+
+// same dropdown but for towns if navarra is selected
+
+
+var provincesSelect = document.getElementById("provinces");
+var townsContainer = document.getElementById("towns-container");
+
+
+provincesSelect.addEventListener("change", function() {
+    if (provincesSelect.value === "Navarra") {
+ 
+        townsContainer.style.display = "block";
+    } else {
+ 
+        townsContainer.style.display = "none";
     }
 });
